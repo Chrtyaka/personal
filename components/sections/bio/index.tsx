f2bc8ccf0936eec537ea1e597d3871a4a66f27cf
Slava Cherkesov <cherkesov@insigne.ru>
2021-09-07 17:00:40 +0300

    feat: Upadate bio avatar card

--- components/sections/bio/index.tsx
+++ components/sections/bio/index.tsx
@@ -5,16 +5,20 @@ export const SectionBio = () => {
 	return (
 		<section id='bio' className='pt-16 pb-32'>
 			<div className='flex w-full pl-64 pr-64'>
-				<div className='w-4/12 flex flex-col items-center justify-start'>
-					<Image
-						className='rounded-full'
-						src={avatarPic}
-						alt='Avatar'
-						width={270}
-						height={270}
-					/>
-					<h2 className='mt-8 font-bold text-2xl'>Slava Cherkesov</h2>
-					<p className='mt-4 text-gray-500'>Frontend Developer | Team Lead</p>
+				<div className='w-3/12 p-8 rounded-3xl shadow-lg flex flex-col items-center justify-start bg-purple-500'>
+					<div className='flex items-center rounded-full border-4 border-white'>
+						<Image
+							className='rounded-full'
+							src={avatarPic}
+							alt='Avatar'
+							width={270}
+							height={270}
+						/>
+					</div>
+					<h2 className='mt-8 font-bold text-2xl text-white'>
+						Slava Cherkesov
+					</h2>
+					<p className='mt-4 text-white'>Frontend Developer | Team Lead</p>
 					<a
 						href='https://insigne.ru/'
 						target='_blank'
@@ -25,7 +29,11 @@ export const SectionBio = () => {
 					</a>
 				</div>
 
-				<div className='w-8/12'>content 2</div>
+				<div className='w-9/12 pl-32'>
+					<h1 className='text-6xl text-purple-500'>
+						👋 Hi! I'm Slava Cherkesov
+					</h1>
+				</div>
 			</div>
 		</section>
 	);
