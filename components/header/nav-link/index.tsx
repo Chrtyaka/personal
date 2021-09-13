67fd599d29ffec253fbcb6fcb377f09cc44c7c9b
Slava Cherkesov <cherkesov@insigne.ru>
2021-09-13 13:04:31 +0300

    feat: Start add darkmode support

--- components/header/nav-link/index.tsx
+++ components/header/nav-link/index.tsx
@@ -3,46 +3,46 @@ import classNames from 'classnames';
 import { NavLinkItem } from '../types';
 
 type NavLinkProps = {
-	active: boolean;
-	onClick: () => void;
-	item: NavLinkItem;
+  active: boolean;
+  onClick: () => void;
+  item: NavLinkItem;
 };
 
 export const NavLink = ({ active, onClick, item }: NavLinkProps) => {
-	const [wrapperClass, setWrapperClass] = useState('');
-	const [linkClass, setLinkClass] = useState('');
-	const defaultWrapperClass =
-		' ml-4 pt-2 pb-2 pl-4 pr-4 rounded-3xl border border-transparent transition-colors cursor-pointer duration-150';
-	const defaultLinkCLass = 'text-base';
+  const [wrapperClass, setWrapperClass] = useState('');
+  const [linkClass, setLinkClass] = useState('');
+  const defaultWrapperClass =
+    'ml-4 pt-2 pb-2 pl-4 pr-4 rounded-md border border-transparent transition-colors cursor-pointer duration-150';
+  const defaultLinkCLass = 'text-base';
 
-	useEffect(() => {
-		setWrapperClass(
-			classNames(
-				defaultWrapperClass,
-				classNames(defaultWrapperClass, {
-					'bg-purple-500': active,
-					'hover:border-purple-500': !active,
-				}),
-			),
-		);
+  useEffect(() => {
+    setWrapperClass(
+      classNames(
+        defaultWrapperClass,
+        classNames(defaultWrapperClass, {
+          'bg-purple-500': active,
+          'hover:border-purple-500': !active,
+        }),
+      ),
+    );
 
-		setLinkClass(
-			classNames(defaultLinkCLass, {
-				'text-white': active,
-				'text-purple-500': !active,
-			}),
-		);
-	}, [active]);
+    setLinkClass(
+      classNames(defaultLinkCLass, {
+        'text-white': active,
+        'text-purple-500': !active,
+      }),
+    );
+  }, [active]);
 
-	return (
-		<a
-			href={item.href}
-			rel={item?.params?.rel}
-			target={item?.params?.target}
-			className={wrapperClass}
-			onClick={onClick}
-		>
-			<span className={linkClass}>{item.name}</span>
-		</a>
-	);
+  return (
+    <a
+      href={item.href}
+      rel={item?.params?.rel}
+      target={item?.params?.target}
+      className={wrapperClass}
+      onClick={onClick}
+    >
+      <span className={linkClass}>{item.name}</span>
+    </a>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			screens: {
-				'3xl': '1920px',
-			},
-			lineHeight: {
-				12: '3rem',
-				13: '3.5rem',
-				14: '4rem',
-			},
-		},
-		zIndex: {
-			1030: 1030,
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1920px',
+      },
+      lineHeight: {
+        12: '3rem',
+        13: '3.5rem',
+        14: '4rem',
+      },
+    },
+    zIndex: {
+      1030: 1030,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
